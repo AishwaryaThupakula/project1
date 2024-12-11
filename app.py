2efd34d5c93540c5023151614b9bab3fbf13a5bf
AishwaryaThupakula <aishwaryathupakula@gmail.com>
2024-12-11 21:54:41 +0530

    Modified

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request, redirect
+from flask_mail import Mail,Message
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "dcartoon560@gmail.com"
+app.config["MAIL_PASSWORD"] = "rzws xlgk jknm xrgq"
+mail = Mail(app)
+
 
 my_client = MongoClient("localhost", 27017)
 my_db = my_client["calci"] # database
@@ -35,24 +43,33 @@ def calculator():
             n1 = int(request.form["num1"])
             opr = request.form["opr"]
             n2 = int(request.form["num2"])
+            msg = Message(subject="calculation", sender="thupakulaaishwarya@gmail.com",
+                        recipients=["dcartoon560@gmail.com"]
+                        )
         
             if opr == "add":
                 res = f"{n1} + {n2} is {n1+n2}"
                 results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                 })
+                msg.body = res
+                mail.send(msg)
                 return render_template("index.html", output=res)
             elif opr == "sub":
                 res = f"{n1} - {n2} is {n1-n2}"
                 results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                 })
+                msg.body = res
+                mail.send(msg)
                 return render_template("index.html", output=res)
             elif opr == "mul":
                 res = f"{n1} x {n2} is {n1*n2}"
                 results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                 })
+                msg.body = res
+                mail.send(msg)
                 return render_template("index.html", output=res)
             elif opr == "div":
                 try:
@@ -60,6 +77,8 @@ def calculator():
                     results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                     })
+                    msg.body = res
+                    mail.send(msg)
                     return render_template("index.html", output=res)
                 except Exception as e:
                     error = "Please change num2 as non-zero"
